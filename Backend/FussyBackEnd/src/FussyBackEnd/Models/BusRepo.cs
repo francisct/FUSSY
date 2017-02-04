6e2c1a1bdb4c4fc7ffdc13eaa50c7381f57924b7
majeanson <marc_jeanson@hotmail.com>
2017-02-04 16:13:09 -0500

    first backend proto

---
+++ Backend/FussyBackEnd/src/FussyBackEnd/Models/BusRepo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FussyBackEnd
+{
+    public class BusRepo
+    {
+        public List<Bus> busList = new List<Bus>();
+
+
+        public void AddBus(Bus bus)
+        {
+            if (busList.Exists(x => x.getId() == bus.getId())) { }
+            else busList.Add(bus);
+
+        }
+        public void RemoveBus(int id)
+        {
+            foreach (Bus bus in busList)
+            {
+                if (bus.getId() == id)
+                {
+                    busList.Remove(bus);
+                }
+            }
+        }
+    }
+}
+
---
+++ Backend/FussyBackEnd/src/FussyBackEnd/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FussyBackEnd
+{
+    public class User
+    {
+        public int id { get; set; }
+        public double longitude { get; set; }
+        public double latitude { get; set; }
+        public Bus bus { get; set; }
+
+        public User(int id, double longitude, double latitude, Bus bus)
+        {
+            this.id = id;
+            this.longitude = longitude;
+            this.latitude = latitude;
+            this.bus = bus;
+        }
+        public User(int id, double longitude, double latitude)
+        {
+            this.id = id;
+            this.longitude = longitude;
+            this.latitude = latitude;
+            this.bus = null;
+        }
+
+        
+
+    }
+}
